8103c0de1611e9a5343583e5e2508a1dae14ca4e
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-01-06 00:53:18 +0530

    Respomsive

--- Components/Projectcontainer.js
+++ Components/Projectcontainer.js
@@ -6,7 +6,7 @@ import Projectviewlink from './Projectviewlink'
 const Projectcontainer = ({project_heading, project_desc, project_img, link, bg = 'transparent'}) => {
     return (
         <div       
-        className='project_content_div flex flex-col gap-4 w-64 h-auto bg-[#F0F0F0] rounded-xl pb-3 dark:bg-[#454545] dark:shadow-own-shadow'>
+        className='project_content_div flex flex-col gap-4 w-64 h-auto bg-[#F0F0F0] rounded-xl pb-3 dark:bg-[#454545] dark:shadow-own-shadow '>
             <Image
                 src={`/Images/${project_img}.png`}
                 width='500'
@@ -14,7 +14,7 @@ const Projectcontainer = ({project_heading, project_desc, project_img, link, bg 
                 className={`project_img logo w-full rounded-t-xl bg-${bg}`}
                 loading="lazy"
             />
-            <div className='flex items-center justify-center flex-col px-4 pb-2 gap-2'>
+            <div className='flex items-center justify-center flex-col px-4 pb-2 gap-2 max-[640px]:px-6 max-[640px]:gap-4 max-[640px]:mt-2'>
                 <h1 id='project___heading' className='project_heading text-center text-3xl uppercase'>{project_heading}</h1>
                 <p id='project__desc' className='text-center text-sm '>{project_desc}</p>
                 <Projectviewlink link={link} />
--- Components/Projectviewlink.js
+++ Components/Projectviewlink.js
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Projectviewlink = ({link}) => {
     return (
-        <Link className='project_link flex items-center justify-center place-self-center py-1 bg-red-500 dark:bg-[#fff] rounded-full w-24 text-center mb-4 text-[#fff] dark:text-red-500 hover:bg-transparent hover:text-red-500 dark:hover:bg-[#ff0000] dark:hover:text-white text-sm gap-1 max-[280px]:text-sm max-[280px]:w-24 max-[280px]:h-8 dark:border-none z-40' href={`https://github.com/ArNAB-0053/${link}`}>
+        <Link className='project_link flex items-center justify-center place-self-center py-1 bg-red-500 dark:bg-[#fff] rounded-full w-24 text-center mb-4 text-[#fff] dark:text-red-500 hover:bg-transparent hover:text-red-500 dark:hover:bg-[#ff0000] dark:hover:text-white text-sm gap-1 max-[280px]:text-sm max-[280px]:w-24 max-[280px]:h-8 dark:border-none z-40 max-[640px]:text-lg' href={`https://github.com/ArNAB-0053/${link}`}>
             <p>View</p>
-            <svg className='rotate-45 max-[280px]:w-4' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <svg className='rotate-45 max-[280px]:w-4 max-[640px]:w-6 max-[640px]:h-6 ' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
             </svg>
         </Link>
