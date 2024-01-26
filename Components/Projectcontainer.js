8f2690761a59aefd5e57d90b7ce4779c90890cda
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-01-27 00:43:52 +0530

    style: Editing in project section

--- Components/Projectcontainer.js
+++ Components/Projectcontainer.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Projectcontainer = ({project_heading, project_desc, project_img, link, bg = 'transparent', fontSize = 'text-3xl'}) => {
     return (
         <Link href={`https://github.com/ArNAB-0053/${link}`}
-        className='project_content_div flex flex-col gap-4 h-auto bg-[#F0F0F0] rounded-xl pb-3 dark:bg-[#454545] dark:shadow-own-shadow max-[640px]:shadow-none max-[640px]:dark:shadow-none max-[640px]:rounded'>
+        className='project_content_div flex flex-col gap-4 h-full w-full bg-[#F0F0F0] rounded-xl pb-3 dark:bg-[#454545] dark:shadow-own-shadow max-[640px]:rounded'>
             <Image
                 src={`/Images/${project_img}.png`}
                 width='500'
