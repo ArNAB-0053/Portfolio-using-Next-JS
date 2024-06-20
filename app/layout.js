63d8931594d352f96e30ca9c0191fc665da6adf6
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-06-20 10:14:05 +0530

    edit: Changing scrollbar style and about me

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   console.clear();
   return (
-    <html lang="en">
-      <body className={`${inter.className} overflow-x-hidden selection:bg-black selection:text-white dark:selection:bg-white dark:selection:text-black dark:scrollbar-track-[#666666] dark:scrollbar-thumb-[#a7a7a7] scrollbar-track-[#dadada] scrollbar-thumb-[#a7a7a7] scrollbar-thin  scrollbar-rounded`}>        
+    <html lang="en" className='scrollbar-thin  scrollbar-rounded'>
+      <body className={`${inter.className} overflow-x-hidden selection:bg-black selection:text-white dark:selection:bg-white dark:selection:text-black dark:scrollbar-track-[#666666]`}>        
         {children}
       </body>
     </html>
