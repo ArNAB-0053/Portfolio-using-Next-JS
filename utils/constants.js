f5c24e276873e5698a3dd75bdd8a3f29c9a4a384
Arnab Bhattacharyya <105587279+ArNAB-0053@users.noreply.github.com>
2026-06-25 20:49:38 +0530

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,2 @@
 export const RESUME_URL =
-  "https://cdn.jsdelivr.net/gh/ArNAB-0053/portfolio-content/PDF/Resume.pdf";
+  "https://cdn.jsdelivr.net/gh/ArNAB-0053/portfolio-content/PDF/Arnab_Bhattacharyya_Resume.pdf";
